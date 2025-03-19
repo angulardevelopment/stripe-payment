a7050b3fa0ff92feea727c16b5915e608a8041ef
Gupta, Sunny <sunnyvales789@gmail.com>
2025-03-20 00:01:05 +0530

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StripePaymentComponent } from './stripe-payment/stripe-payment.component';
+import { FinalStepComponent } from './final-step/final-step.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'FinalStepComponent', component: FinalStepComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, Routes } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'payment';
+  routes: Routes = [];
+
+  constructor(private router: Router) {}
+
+  ngAfterViewInit(): void {
+    this.routes = this.router.config;
+  }
+
 }
